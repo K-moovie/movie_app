ae3b41bce7a3ec6f8742b7fb264e9eca14289aeb
K-moovie <kyha97@naver.com>
2020-04-15 21:31:29 +0900

    2nd Day

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
+// <App />과 같은 것은 component, react는 component와 함께 동작한다.
+// component는 HTML을 반환하는 함수
+// jsx는 javascript안의 HTML이다. 
+// react application은 한 번에 하나의 component만 rendering할 수 있다.
+// 따라서 모든 것은 application (App) 안에 들어가야 한다. 
+// App안에서 많은 component를 넣을 수 있고, 이러한 component안에 더 많은 component를 import할 수 있다. 
