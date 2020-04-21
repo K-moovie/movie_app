be4e8192806863b57af2c886894543c93ebc7e82
K-moovie <kyha97@naver.com>
2020-04-21 20:58:23 +0900

    4th Day

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
     state = {
-        isLoading: true
+        isLoading: true,
+        movies: []
     };
 
-    render() {
-        return <div></div>;
+    getMovies = async () => { // 비동기(async), await 기다려라
+        const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
     }
+
+    componentDidMount() {
+        // setTimeout(() => {
+        //    this.setState({isLoading: false});
+        //}, 5000);
+        // timeout은 JavaScript 함수
+        this.getMovies();
+    }
+
+    render() {
+        const { isLoading } = this.state;
+        return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    // ? : 연산자는 JavaScript
+    };
 }
 
 export default App;
